4499294f23122afd6f770b1da5bea7bcc17b4dbf
Muhammad Asif Javed <axif.javed@gmail.com>
2017-11-15 19:55:09 +0000

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,9 @@ module.exports = {
   ],
   devServer: {
     contentBase: './client',
-    hot: true
+    hot: true,
+    host: process.env.IP || "0.0.0.0",
+    port: process.env.PORT || 3000,
+    disableHostCheck: true
   }
 }
